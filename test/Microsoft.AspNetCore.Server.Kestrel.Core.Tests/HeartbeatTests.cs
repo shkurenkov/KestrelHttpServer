7a4e069100d76df6e828e930556524538a023420
Stephen Halter <halter73@gmail.com>
2017-04-11 12:13:39 -0700

    WIP

---
+++ test/Microsoft.AspNetCore.Server.Kestrel.Core.Tests/HeartbeatTests.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using System.Threading;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Infrastructure;
+using Microsoft.AspNetCore.Testing;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Server.Kestrel.Core.Tests
+{
+    public class HeartbeatTests
+    {
+        [Fact]
+        public void BlockedHeartbeatLoggedAsError()
+        {
+            var systemClock = new MockSystemClock();
+            var heartbeatInterval = TimeSpan.FromMilliseconds(10);
+            var heartbeatHandler = new Mock<IHeartbeatHandler>();
+            var kestrelTrace = new Mock<IKestrelTrace>();
+            var handlerMre = new ManualResetEventSlim();
+            var traceMre = new ManualResetEventSlim();
+
+            heartbeatHandler.Setup(h => h.OnHeartbeat(systemClock.UtcNow)).Callback(() => handlerMre.Wait());
+            kestrelTrace.Setup(t => t.TimerSlow(heartbeatInterval, systemClock.UtcNow)).Callback(() => traceMre.Set());
+
+            using (new Heartbeat(new[] {heartbeatHandler.Object}, systemClock, kestrelTrace.Object, heartbeatInterval))
+            {
+                Assert.True(traceMre.Wait(TimeSpan.FromSeconds(1)));
+                handlerMre.Set();
+            }
+        }
+
+        [Fact]
+        public void ExceptionFromHeartbeatHandlerLogged()
+        {
+            var systemClock = new MockSystemClock();
+            var heartbeatHandler = new Mock<IHeartbeatHandler>();
+            var kestrelTrace = new TestKestrelTrace();
+            var ex = new Exception();
+
+            heartbeatHandler.Setup(h => h.OnHeartbeat(systemClock.UtcNow)).Throws(ex);
+
+            using (new Heartbeat(new[] { heartbeatHandler.Object }, systemClock, kestrelTrace))
+            {
+                // Heartbeat as a "dueTime" of zero, so a second should be more than enough time
+                // for an error to be logged.
+                Assert.True(kestrelTrace.Logger.MessageLoggedTask.Wait(TimeSpan.FromSeconds(1)));
+            }
+
+            Assert.Equal(ex, kestrelTrace.Logger.Messages.Single(message => message.LogLevel == LogLevel.Error).Exception);
+        }
+    }
+}
